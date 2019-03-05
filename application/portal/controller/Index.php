dec7b549edfd7ed692b9637e3f282935f5aefbd5
Sunny <363216816@qq.com>
2019-03-05 12:42:04 +0800

    完善内容录入表单3-4

---
+++ application/portal/controller/Index.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\portal\controller;
+
+use think\Controller;
+
+class Index extends Controller
+{
+    public function index()
+    {
+        return view('index');
+    }
+}
---
+++ config/portal/template.php
@@ -0,0 +1,9 @@
+<?php
+//模板配置
+return [
+    'tpl_replace_string'  =>  [
+        '__PORTAL__'=>'/static/portal',
+        '__PLUGINS__' => '/static/plugins',
+        '__BOOTSTRAP__' => '/static/admin/bootstrap',
+    ]
+];
